8decf16aa2e8e348938e6f0c6ec43bae4c0b0f8b
Mohammad Zeeshan <166004209+mohammad-zeeshan2024@users.noreply.github.com>
2024-10-21 16:22:29 +0000

    name edit

---
+++ 1_basics/1_variable.js
@@ -0,0 +1,27 @@
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+const accountId = 123456    // valu not changeable 
+let accountEmail = "abcd@gmail.com"
+var accountPassword = "09876"
+accountCity = "Siwan"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+// accountId = 2222  // not allowed
+
+accountEmail = "zxcv@gmail.com"
+accountPassword = "3456"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
