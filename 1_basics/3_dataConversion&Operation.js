a275eaca47141a9d64c979245147ab1652e40f97
Mohammad Zeeshan <166004209+mohammad-zeeshan2024@users.noreply.github.com>
2024-10-23 08:45:51 +0000

    data conversion and Operation

---
+++ 1_basics/3_dataConversion&Operation.js
@@ -0,0 +1,38 @@
+let score = "zeeshan"
+
+console.log(typeof score);
+console.log(typeof(score));  //second way of write
+
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);  // this console show Number
+console.log(valueInNumber); // this console show NaN
+
+
+
+// "33" => 33  // convert string into number
+// "33abc" => NaN  // console show NaN
+//  true => 1; false => 0
+
+
+
+let isLoggedIn = "zeeshan"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
