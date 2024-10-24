5f46fd8d0223150a56953703d4f2ea735ced7fd3
Mohammad Zeeshan <166004209+mohammad-zeeshan2024@users.noreply.github.com>
2024-10-24 18:34:12 +0000

    numbers and Maths

--- 1_basics/6_nums_maths.js
+++ 1_basics/6_nums_maths.js
@@ -15,7 +15,8 @@ console.log(balance.toFixed(2));
 
 let num = new Number(12.546783)
 
-console.log(num.toFixed(2));  // with the help of toFixrd() method its allow the Number of digits after the decimal point. Ex -> output is 12.55
+console.log(num.toFixed(2));  
+// with the help of toFixrd() method its allow the Number of digits after the decimal point. Ex -> output is 12.55
 
 console.log(num.toFixed(4));  // output is -> 12.5468
 
@@ -38,9 +39,11 @@ console.log(numb.toPrecision(6)); // Output: "123.457"
 
 const hundreds = 1000000
 
-console.log(hundreds.toLocaleString());  // if we only call toLocaleString() method value we provide US standards values.
+console.log(hundreds.toLocaleString());  
+// if we only call toLocaleString() method value we provide US standards values.
 
-console.log(hundreds.toLocaleString('en-IN'));  // if we call toLocaleString('en-IN) method value we provide indian standards values.
+console.log(hundreds.toLocaleString('en-IN'));  
+// if we call toLocaleString('en-IN) method value we provide indian standards values.
 
 
 
@@ -48,17 +51,21 @@ console.log(hundreds.toLocaleString('en-IN'));  // if we call toLocaleString('en
 
 
 console.log(Math);
-console.log(Math.abs(-5));  // this method Maths.abs() method we provide positive number. 
+console.log(Math.abs(-5));  
+// this method Maths.abs() method we provide positive number. 
 // Return -> 4
 
-console.log(Math.round(4.6));  // this method round the number of nearest integer.
+console.log(Math.round(4.6));  
+// this method round the number of nearest integer.
 // if fraction part of the number is 0.5 or greater, the argument is rounded up to the next integer.
 // if fraction part is less than 0.5, the argument is rounded down.
 
-console.log(Maths.ceil(4.2));  // this methode is used to round a number up the next largest intiger, even if the number is already an integer.
+console.log(Math.ceil(4.2));  
+// this methode is used to round a number up the next largest intiger, even if the number is already an integer.
 // this method is useful when you always want to round a number upwards.
 
-console.log(Math.floor(4.9));  // this method is useful when you always want to round a number downwards. // Return -> 4
+console.log(Math.floor(4.9));  
+// this method is useful when you always want to round a number downwards. // Return -> 4
 
 console.log(Math.min(7,8,9,5,7));
 console.log(Math.max(7,8,9,5,7));
