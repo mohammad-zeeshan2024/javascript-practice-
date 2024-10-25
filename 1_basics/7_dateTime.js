7f708f95099cb9068db035c09c450da8eb8ac834
Mohammad Zeeshan <166004209+mohammad-zeeshan2024@users.noreply.github.com>
2024-10-25 13:57:46 +0000

    Date and time

--- 1_basics/7_dateTime.js
+++ 1_basics/7_dateTime.js
@@ -18,6 +18,34 @@ console.log(myCreatedDate.toDateString());
 let anotherMyCreatedDate = new Date(2022, 2, 12, 5, 7)
 console.log(anotherMyCreatedDate.toLocaleString());
 
+let myTimeStamp = Date.now()  // this function returns milliseconds 1 jan 1970 to till date
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));  // convert milliseconds to seconds.
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());  // month started from 0 index (0 -> january) and 11 index (11 -> December).
+
+console.log(newDate.getDay());  // day started monday (monday -> 1)
+
+
+newDate.toLocaleString('default', {   // we can also use as object.
+    weekday : "long",
+
+})
+
+
+
+
+
+
+
+
 
 
 
