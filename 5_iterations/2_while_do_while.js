ff3d3bfbddb1ccabff9e16bff4bea3fd3b2c16e3
Mohammad Zeeshan <166004209+mohammad-zeeshan2024@users.noreply.github.com>
2024-10-31 05:54:53 +0000

    while do while loop

---
+++ 5_iterations/2_while_do_while.js
@@ -0,0 +1,43 @@
+// while loop
+
+let i = 0
+while (i <= 20) {
+    console.log(i);
+    i = i + 2
+    
+}
+
+
+
+let arr = ["apple", "banana", "chery"]
+
+let newArr = 0
+while (newArr < arr.length) {
+    console.log(arr[newArr]);
+    newArr++
+}
+
+
+
+// do while loop
+
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
+// interesting case
+
+
+
+let score2 = 0
+
+do {
+    console.log(`score is ${score2}`);
+    score2++
+} while (score2 <=10);
+
+
+
