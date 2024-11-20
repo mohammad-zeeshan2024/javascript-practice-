1d50b8e83dcd8eaf81439a8768fe2e6b800e8b72
Mohammad Zeeshan <166004209+mohammad-zeeshan2024@users.noreply.github.com>
2024-11-20 17:35:06 +0000

    Color Scheme Switcher

--- 7_project/1_change-web-color/script.js
+++ 7_project/1_change-web-color/script.js
@@ -2,7 +2,7 @@ const btn = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 
 btn.forEach(function (buttons) {
-    log(buttons);  // here we can access all buttons. 
+    console.log(buttons);  // here we can access all buttons. 
     buttons.addEventListener('click',function(event){
         console.log(event);
         console.log(event.target);  
